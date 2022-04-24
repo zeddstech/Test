7da102fda4950bbc056a15d316dd21f04e9d8a19
zeddstech <39128666+zeddstech@users.noreply.github.com>
2022-04-24 09:18:08 -0500

    Update wonda_mail_bot.php

--- wonda_mail_bot.php
+++ wonda_mail_bot.php
@@ -7,11 +7,11 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require 'vendor/autoload.php';
-print_r($_POST);die;
+/*print_r($_POST);die;
 $content="";
 foreach($_POST as $key=>$value){
     $content.=$key.": ".$value;
-}
+}*/
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
@@ -19,15 +19,15 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'mail.privateemail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     //SMTP username
-    $mail->Password   = 'secret';                               //SMTP password
+    $mail->Username   = 'webmaster@hex3x.live';                     //SMTP username
+    $mail->Password   = 'ynvNyLCXdbaa';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->setFrom('webmaster@hex3x.live', 'Webmaster');
     $mail->addAddress('JosephRHopkins@yandex.com', 'Joseph R Hopkins');     //Add a recipient
 
 
