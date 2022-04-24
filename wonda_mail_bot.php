aa72bade91f2b0f92db4ee587ceb3103669e8094
zeddstech <39128666+zeddstech@users.noreply.github.com>
2022-04-24 09:22:27 -0500

    Update wonda_mail_bot.php

--- wonda_mail_bot.php
+++ wonda_mail_bot.php
@@ -7,11 +7,11 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require 'vendor/autoload.php';
-/*print_r($_POST);die;
+//print_r($_POST);die;
 $content="";
 foreach($_POST as $key=>$value){
     $content.=$key.": ".$value;
-}*/
+
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
